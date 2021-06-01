bacdb4060d34296640ec63903c57a1336b7817b9
Fernando Hernan Egas <fernandoegas@MBP-de-Fernando.fibertel.com.ar>
2021-06-01 17:29:18 -0300

    Updates

---
+++ precache-manifest.32fd5a1ff546083bb247b5e09748ff5a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79abaaaed30f8451fe65eb8b60209aa5",
+    "url": "/KepperApp/index.html"
+  },
+  {
+    "revision": "b4f4aa1db489a5571c18",
+    "url": "/KepperApp/static/js/2.cd56f810.chunk.js"
+  },
+  {
+    "revision": "0b874d76dbd91ff12270",
+    "url": "/KepperApp/static/js/main.73126b7f.chunk.js"
+  },
+  {
+    "revision": "278736411c273dc1cb03",
+    "url": "/KepperApp/static/js/runtime-main.010c0ac7.js"
+  }
+]);
